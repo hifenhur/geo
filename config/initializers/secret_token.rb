c43d3dd58cef7f8ad651231ee4127d12dfca0bfc
Ben-Hur B. Oliveira <benhur@engenhariadesoftware.inf.br>
2013-08-06 22:15:57 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geomarketing::Application.config.secret_token = 'f210479d96506c982e9db581adb0306ce7da291c586570bbaadcc3f389e2810bba96d1f7e7245442a93183d38e04db867e80a3c97b580db633150df8c801abdf'
